6b9a457b3ee21627a5669fead3b6d231bee0fb3b
Mariya <mariya.chirva@gmail.com>
2021-03-14 20:32:34 +0300

    Выделен класс для управдения сессиями

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,18 +12,18 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="Masha", middlename="Alexandrovna", lastname="Chirva", nickname="Mmm",
                     title="Test", company="Python", address="Address", home="55-55-55", mobile="11-11-11", work="no",
                     fax="no", email="mariya.chirva@gmail.com", homepage="no", bday="18", bmonth="October", byear="1933",
                     aday="18", amonth="December", ayear="1963", address2="no", phone2="no", notes="no"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="",
                     title="", company="", address="", home="", mobile="", work="",
                     fax="", email="", homepage="", bday="", bmonth="-", byear="",
                     aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
-    app.logout()
+    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="New group 1", header="New group 1", footer="New group 1"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()

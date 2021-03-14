a145ac7c51be0ba7c97ff161dbf972a5d1ea6de1
Mariya <mariya.chirva@gmail.com>
2021-03-14 21:08:08 +0300

    Созданы классы-помощники и создан класс-менеджер, который инициализирует всех помощников

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,17 +13,17 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Masha", middlename="Alexandrovna", lastname="Chirva", nickname="Mmm",
-                    title="Test", company="Python", address="Address", home="55-55-55", mobile="11-11-11", work="no",
-                    fax="no", email="mariya.chirva@gmail.com", homepage="no", bday="18", bmonth="October", byear="1933",
-                    aday="18", amonth="December", ayear="1963", address2="no", phone2="no", notes="no"))
+    app.contact.create(Contact(firstname="Masha", middlename="Alexandrovna", lastname="Chirva", nickname="Mmm",
+                               title="Test", company="Python", address="Address", home="55-55-55", mobile="11-11-11", work="no",
+                               fax="no", email="mariya.chirva@gmail.com", homepage="no", bday="18", bmonth="October", byear="1933",
+                               aday="18", amonth="December", ayear="1963", address2="no", phone2="no", notes="no"))
     app.session.logout()
 
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="", middlename="", lastname="", nickname="",
-                    title="", company="", address="", home="", mobile="", work="",
-                    fax="", email="", homepage="", bday="", bmonth="-", byear="",
-                    aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
+    app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
+                               title="", company="", address="", home="", mobile="", work="",
+                               fax="", email="", homepage="", bday="", bmonth="-", byear="",
+                               aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
     app.session.logout()

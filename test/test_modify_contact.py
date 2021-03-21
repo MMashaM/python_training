c711cf0d41eed5523b50ca1107049039c29c18ad
Mariya <mariya.chirva@gmail.com>
2021-03-21 17:39:11 +0300

    Добавлен тест из видео test_modify_group_trainer.py функции login/logout  вынесены в фикстуру

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,9 +2,7 @@
 from model.contact import Contact
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(firstname="1", middlename="2", lastname="3", nickname="",
                                title="", company="", address="", home="", mobile="", work="",
                                fax="", email="", homepage="", bday="", bmonth="-", byear="",
                                aday="", amonth="-", ayear="", address2="", phone2="", notes=""))
-    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,6 +2,4 @@
 from model.group import Group
 
 def test_modify_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group 2", header="New group 3", footer="New group 4"))
-    app.session.logout()

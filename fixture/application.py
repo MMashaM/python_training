6d1f3e964437d2ff5f717ebaface931c44807be8
Mariya <mariya.chirva@gmail.com>
2021-04-02 01:11:46 +0300

    Правка фикстуры. Вынесли в параметры типы браузера и url сайта

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self, browser="firefox"):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -20,6 +20,7 @@ class Application:
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -32,7 +33,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook") and len(wd.find_elements_by_name("user")) > 0):
             # wd.find_element_by_link_text("home").click()
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def return_to_homepage(self):
         wd = self.wd

35d691bfb5656829da573f316667f630846e6688
Mariya <mariya.chirva@gmail.com>
2021-04-04 17:03:21 +0300

    поменяла тест модификации группы на сравнение с БД

--- fixture/group.py
+++ fixture/group.py
@@ -94,6 +94,18 @@ class GroupHelper:
         self.return_to_groups_name()
         self.group_cache = None
 
+    def modify_group_by_id(self, id, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        self.select_group_by_id(id)
+        # submit edit
+        wd.find_element_by_name("edit").click()
+        self.fill_group_form(group)
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_name()
+        self.group_cache = None
+
     def return_to_groups_name(self):
         wd = self.app.wd
         # wd.find_element_by_link_text("group page").click()

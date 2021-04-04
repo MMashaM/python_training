48ac6ddf94db925e50c7420f043068d41e1b7759
Mariya <mariya.chirva@gmail.com>
2021-04-04 20:07:53 +0300

    Задание №21. test_contacts_home_page.py

--- test/test_contacts_home_page.py
+++ test/test_contacts_home_page.py
@@ -1,17 +1,22 @@
 import re
-from random import randrange
+from model.contact import Contact
 
 
-# сравнение данных контакта с главной страницы и данных с формы редактирования контакта
 def test_contacts_home_page(app, db):
-    all_index = randrange(app.contact.count())
-    contact_from_home_page = db.get_contact_list()[all_index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(all_index)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="test"))
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    assert len(contact_from_home_page) == len(contact_from_db)
+    #assert sorted(contact_from_home_page, key=Contact.id_or_max()) == sorted(contact_from_db, key=Contact.id_or_max())
+    for i in range(len(contact_from_home_page)):
+        contact_from_home_page_by_index = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)[i]
+        contact_from_db_by_index = db.get_contact_list()[i]
+        assert contact_from_home_page_by_index.firstname == contact_from_db_by_index.firstname
+        assert contact_from_home_page_by_index.lastname == contact_from_db_by_index.lastname
+        assert contact_from_home_page_by_index.address == contact_from_db_by_index.address
+        assert contact_from_home_page_by_index.all_mail == merge_emails_like_on_home_page(contact_from_db_by_index)
+        assert contact_from_home_page_by_index.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db_by_index)
 
 
 def clear(s):

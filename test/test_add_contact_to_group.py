f77b11fcdc4d98e6cb2448150108c817b4ea3282
Mariya <mariya.chirva@gmail.com>
2021-04-07 21:18:07 +0300

    Задание №22. Доработано по замечанию "Для теста добавления контакта в группу ещё реализуйте проверку предусловий, а также проверку того, что контакт был добавлен в нужную группу."

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 import random
+from model.contact import Contact
+from model.group import Group
 
 
 def test_add_contact_to_group(app, ormdb):
+    # проверка предусловий
+    if len(ormdb.get_contact_list()) == 0:
+        app.contact.create(Contact(firstname="test"))
+    if len(ormdb.get_group_list()) == 0:
+        app.group.create(Group(name="test"))
     exception_group_list = []
     contact = random.choice(ormdb.get_contact_list())
     list_groups_for_contact = ormdb.get_groups_for_contact(contact)
@@ -17,4 +24,5 @@ def test_add_contact_to_group(app, ormdb):
         index = random.randrange(len(exception_group_list))
         group = list_all_groups[index]
     app.contact.add_contact_to_group(contact, group)
-    #assert group in ormdb.get_groups_for_contact(contact)
+    # проверяем, что контакт добавлен в группу
+    assert group in ormdb.get_groups_for_contact(contact)
